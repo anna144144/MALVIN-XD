25c64d8d268a3522761dbc2f46edb290a30dc974
anna144144 <annastaziajames287@gmail.com>
2025-07-24 00:52:46 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "255716204767", // 0716204767
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
